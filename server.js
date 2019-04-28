9ece11ac1579e952136378354c05ea8ed721fafb
Pankaj Shinde <p10shinde@gmail.com>
2019-04-28 16:22:16 +0530

    fixed serverJS

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require('path');
 
-app.use(expres.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
 
@@ -13,4 +13,4 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-console.log('Server started on!');
+console.log('Server started!');
